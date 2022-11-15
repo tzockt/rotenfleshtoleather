a43bd142ce3315ab354326baaba430bbddf5cde4
TZockt <info@tzockt.de>
2022-11-15 19:05:17 +0100

    Fix Shit

--- src/main/java/de/tzockt/rottenfleshtoleather/rottenfleshtoleather.java
+++ src/main/java/de/tzockt/rottenfleshtoleather/rottenfleshtoleather.java
@@ -2,16 +2,16 @@ package de.tzockt.rottenfleshtoleather;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 public class rottenfleshtoleather extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Rotten Flesh to Leather aktiviert");
+        getLogger().info("§6Rotten Flesh to Leather aktiviert");
 
-    ShapedRecipe rottenfleshtoleather = new ShapedRecipe(new ItemStack(Material.LEATHER, 1));
+    ShapedRecipe rottenfleshtoleather = new ShapedRecipe(new NamespacedKey("rottenfleshtoleather", "leather"), new ItemStack(Material.LEATHER, 1));
     rottenfleshtoleather.shape("RRR", "RRR", "RRR" );
     rottenfleshtoleather.setIngredient('R', Material.ROTTEN_FLESH);
     Bukkit.addRecipe(rottenfleshtoleather);
@@ -19,6 +19,6 @@ public class rottenfleshtoleather extends JavaPlugin {
     }
     @Override
     public void onDisable() {
-        getLogger().info("Rotten Flesh to Leather deaktiviert");
+        getLogger().info("§4Rotten Flesh to Leather deaktiviert");
     }
 }
