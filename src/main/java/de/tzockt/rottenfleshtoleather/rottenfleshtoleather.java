a2e5448a3932104e998595983e9e2ad5dfe0e337
TZockt <info@tzockt.de>
2023-06-26 22:17:33 +0200

    Update to JAVA 17 and PaperMC 20.1

--- src/main/java/de/tzockt/rottenfleshtoleather/rottenfleshtoleather.java
+++ src/main/java/de/tzockt/rottenfleshtoleather/rottenfleshtoleather.java
@@ -6,17 +6,21 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public class rottenfleshtoleather extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("§6Rotten Flesh to Leather aktiviert");
 
-    ShapedRecipe rottenfleshtoleather = new ShapedRecipe(new NamespacedKey("rottenfleshtoleather", "leather"), new ItemStack(Material.LEATHER, 1));
-    rottenfleshtoleather.shape("RRR", "RRR", "RRR" );
-    rottenfleshtoleather.setIngredient('R', Material.ROTTEN_FLESH);
-    Bukkit.addRecipe(rottenfleshtoleather);
-    
+        NamespacedKey key = new NamespacedKey(this, "leather"); // Erstelle einen NamespacedKey für das Rezept
+
+        ShapedRecipe rottenFleshToLeather = new ShapedRecipe(key, new ItemStack(Material.LEATHER, 1));
+        rottenFleshToLeather.shape("RRR", "RRR", "RRR");
+        rottenFleshToLeather.setIngredient('R', Material.ROTTEN_FLESH);
+
+        Bukkit.addRecipe(rottenFleshToLeather);
     }
+
     @Override
     public void onDisable() {
         getLogger().info("§4Rotten Flesh to Leather deaktiviert");
